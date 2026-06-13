c7286b472d8105f1c190073953d41b8d13c21539
PranavSodani <144207193+PranavSodani@users.noreply.github.com>
2026-06-13 09:53:00 +0530

    Added the load_dotenv()

--- backend/database.py
+++ backend/database.py
@@ -4,6 +4,8 @@ from sqlalchemy.orm import declarative_base
 from dotenv import load_dotenv
 import os 
 
+load_dotenv()
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
